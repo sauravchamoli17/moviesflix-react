b7a0c25d39ea217b77befc6a784acd69cce3b157
Saurav Chamoli <saurav.chamoli@ebizondigital.com>
2022-05-02 14:39:48 +0530

    Updates

---
+++ precache-manifest.0b9deacfb306b0b6122f8bf3d92f71b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe7f18e8b918ed957b95337802fe420",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e3ebe82002781809a88",
+    "url": "./static/css/main.d09bb36c.chunk.css"
+  },
+  {
+    "revision": "7f1ab1574f27de3e8434",
+    "url": "./static/js/2.21ba9110.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.21ba9110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3ebe82002781809a88",
+    "url": "./static/js/main.b9351d41.chunk.js"
+  },
+  {
+    "revision": "ca0d077a788950b93618",
+    "url": "./static/js/runtime-main.febabb0c.js"
+  },
+  {
+    "revision": "73f0bcaf5385e0980e347b81fcb2e93a",
+    "url": "./static/media/no-image.73f0bcaf.png"
+  }
+]);
